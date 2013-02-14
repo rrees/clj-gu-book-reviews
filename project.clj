02696b0d7cdef53b95b9cc327446f81337c637b1
Robert Rees <Robert Rees>
2013-02-14 22:23:21 +0000

    Fixes Ring Jetty dependency

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "Guardian book reviews"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [compojure "1.1.5"]]
+                 [compojure "1.1.5"]
+                 [ring/ring-jetty-adapter "1.1.0"]]
   :plugins [[lein-ring "0.8.2"]]
   :min-lein-version "2.0.0"
   :ring {:handler clj-gu-book-reviews.handler/app}
