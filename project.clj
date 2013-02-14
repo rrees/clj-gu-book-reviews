8ac141bdc2c92c8d37acb51cb41b479ea3c8cf81
Robert Rees <Robert Rees>
2013-02-14 22:18:46 +0000

    Adds a min lein value, trying to fix a Heroku issue

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.5"]]
   :plugins [[lein-ring "0.8.2"]]
+  :min-lein-version "2.0.0"
   :ring {:handler clj-gu-book-reviews.handler/app}
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.3"]]}})
