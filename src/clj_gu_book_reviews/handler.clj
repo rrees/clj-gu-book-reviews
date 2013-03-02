edc767a30c2bc69686ab10977d309b5c71f63530
Robert Rees <Robert Rees>
2013-03-02 22:20:31 +0000

    Creates a stripped title and uses it, escapes the title and standfirst

--- src/clj_gu_book_reviews/handler.clj
+++ src/clj_gu_book_reviews/handler.clj
@@ -7,12 +7,15 @@
 
 (def book-reviews-url "http://content.guardianapis.com/search?tag=type%2Farticle%2Ctone%2Freviews&section=books&page-size=20&format=json&show-fields=standfirst")
 
+(defn clean-title [item]
+	(assoc item :strippedTitle (.replaceFirst (:webTitle item) " . review", "")))
+
 (defn retrieve-data []
 	(let [url (new java.net.URL book-reviews-url)
 		content (slurp (.getContent url))
 		json-data (json/parse-string content true)
 		results (get-in json-data [:response :results])]
-		{:reviews results}))
+		{:reviews (map clean-title results)}))
 
 (defroutes app-routes
   (GET "/" [] (moustache/render-resource "templates/index.html" (retrieve-data)))
