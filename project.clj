b6ca790155376f68b219cee91b1d7740cafb9a6f
Robert Rees <Robert Rees>
2013-03-02 21:32:21 +0000

    Adds templating, Typeplate and some basic layou

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.5"]
-                 [ring/ring-jetty-adapter "1.1.0"]]
+                 [ring/ring-jetty-adapter "1.1.0"]
+                 [de.ubercode.clostache/clostache "1.3.1"]]
   :plugins [[lein-ring "0.8.2"]]
   :min-lein-version "2.0.0"
   :ring {:handler clj-gu-book-reviews.handler/app}
--- src/clj_gu_book_reviews/handler.clj
+++ src/clj_gu_book_reviews/handler.clj
@@ -1,10 +1,12 @@
 (ns clj-gu-book-reviews.handler
   (:use compojure.core)
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [clostache.parser :as moustache]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (moustache/render-resource "templates/index.html" {:reviews [{:title "Murder Mile"} {:title "Slugs and lettuces"}]}))
+  (route/resources "/static/")
   (route/not-found "Not Found"))
 
 (def app
